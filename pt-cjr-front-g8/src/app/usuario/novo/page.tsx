192824f785174ecb73f66634dc2f1acc3e5f6604
VitorGuedes22 <studyprofessional18@gmail.com>
2025-06-09 10:21:10 -0300

    hotfix: styleByMainForCenter

--- pt-cjr-front-g8/src/app/usuario/novo/page.tsx
+++ pt-cjr-front-g8/src/app/usuario/novo/page.tsx
@@ -4,29 +4,23 @@ import Image from 'next/image';
 
 const novoUsuarioPage = () => {
   return (
-    <>
+ 
       <main style={{ 
-        backgroundColor: 'black', 
-        margin: '0px',
-        padding: '0px',
-        boxSizing: 'border-box',
-        minHeight: 'fit-content',
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'space-between' }}>
+        justifyContent: 'center' }}>
           
           <Image
             src="/image/usuarioImagePage.png"
             alt="alunos"
             width={800}
             height={600}
-            style={{ maxWidth: '100%', height: 'auto' }} 
+            style={{ maxWidth: '50%', height: 'auto' }} 
           />
           
           <FormBox />
 
       </main>
-    </>
     
   )
 }
