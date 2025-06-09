597f882e108e12b02e5a2b14ed98028b3c0ebb2c
Marcio Vieira <marciovds07@gmail.com>
2025-06-09 00:57:17 -0300

    feat- Pagina de login em CSS, falta atualizar para tailwind

--- pt-cjr-front-g8/src/app/page.tsx
+++ pt-cjr-front-g8/src/app/page.tsx
@@ -1,14 +1,34 @@
 import Image from "next/image";
+import FormBox from "./components/formulario/loginBox";
 import Link from "next/link";
 
 //development 
-
+//PAGINA INICIAL/LOGIN
 export default function Home() {
   return (
-    <main>
-      <h1>Hello</h1>
-      <Link href={"/usuario"}>Novo user</Link>
-    </main>
+    <main style={{
+      backgroundColor: 'black', 
+      margin: '0px',
+      padding: '0px',
+      boxSizing: 'border-box',
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between'}}>
+      <div style={{ width: '50%', height: '100%', position: 'relative' }}>
+        <Image
+          src="/image/usuarioImagePage.png"
+          alt="alunos"
+          fill
+          style={{ objectFit: 'cover' }} // ou 'contain', dependendo do efeito desejado
+        />
+      </div>
 
+      <FormBox /> 
+    </main>
   );
 }
+ 
+
+/*<h1>Hello</h1>
+      <Link href={"/usuario"}>Novo user</Link> */
