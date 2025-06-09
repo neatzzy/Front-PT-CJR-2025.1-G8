99463507bf5aafe18656b15913128bf1170e910e
VitorGuedes22 <studyprofessional18@gmail.com>
2025-06-09 15:33:02 -0300

    fix: resolve problem by heigth of elements

--- pt-cjr-front-g8/src/app/usuario/novo/page.tsx
+++ pt-cjr-front-g8/src/app/usuario/novo/page.tsx
@@ -10,17 +10,41 @@ const novoUsuarioPage = () => {
         padding: '0px',
         flexDirection: 'row',
         justifyContent: 'center',
-        height: '100vh' }}>
-          
+        height: 'fit-content',
+        minHeight: '100vh',
+      }}>
+        <div style={{
+          backgroundColor: 'blue',
+          width : '50%',
+          height: 'auto',
+          position: 'relative'
+        }}>
+
           <Image
             src="/image/usuarioImagePage.png"
             alt="alunos"
-            width={800}
-            height={600}
-            style={{ width: '50%', height: 'auto' }} 
+            fill // faz a imagem ocupar toda a div
+            style={{
+              objectFit: 'cover'
+            }}
           />
-          
-          <FormBox />
+
+          <h1
+            style={{
+              position: 'absolute',
+              top: '100px',
+              left: '50%',
+              transform: 'translateX(-50%)',
+              color: 'white',
+              fontSize: '3rem',
+              textShadow: '3px 3px 8px rgba(0, 0, 0, 0.7)',
+            }}
+          >
+      Cadastro Usuário
+    </h1>
+        </div>
+        
+        <FormBox />
 
       </main>
     
