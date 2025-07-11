bd6f8917fe31f9c4c689c9a3c00708dc066ba296
VitorGuedes22 <studyprofessional18@gmail.com>
2025-07-11 10:58:39 -0300

    feat : add config de tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
